d074336572f883e840d871618bc80bbcb6033bb5
monda00 <monda0524@gmail.com>
2023-05-01 13:57:12 +0900

    add sample project created by spring boot cli

---
+++ sample-maven-spring-boot-app/src/main/java/com/example/samplemavenspringbootapp/SampleMavenSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplemavenspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMavenSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMavenSpringBootAppApplication.class, args);
+	}
+
+}
