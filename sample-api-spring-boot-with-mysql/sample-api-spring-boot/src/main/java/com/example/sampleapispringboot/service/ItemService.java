7b7553d316cddfef4a81c0976858f01d59a821be
monda00 <monda0524@gmail.com>
2023-05-13 14:31:52 +0900

    update sample-api-spring-boot-with-mysql

--- sample-api-spring-boot-with-mysql/sample-api-spring-boot/src/main/java/com/example/sampleapispringboot/service/ItemService.java
+++ sample-api-spring-boot-with-mysql/sample-api-spring-boot/src/main/java/com/example/sampleapispringboot/service/ItemService.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,26 +16,46 @@ public class ItemService {
   @Autowired
   private ItemRepository itemRepository;
 
+  @Cacheable("getItems")
   public List<Item> getAllItems() {
     List<Item> allItems = new ArrayList<>();
+
+    try {
+      Thread.sleep(3000);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     itemRepository.findAll().forEach(allItems::add);
     return allItems;
   }
 
+  @Cacheable(value = "getItem", key = "#p0")
   public Optional<Item> getItem(Long itemId) {
+    try {
+      Thread.sleep(3000);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     return itemRepository.findById(itemId);
   }
 
+  @CacheEvict(value = "getItems", allEntries = true)
   public void addItem(Item item) {
     itemRepository.save(item);
   }
 
+  @Caching(evict = {
+      @CacheEvict(value = "getItem", key = "#p0"), @CacheEvict(value = "getItems", allEntries = true)
+  })
   public void updateItem(Long itemId, Item item) {
     if (itemRepository.findById(itemId).get() != null) {
       itemRepository.save(item);
     }
   }
 
+  @Caching(evict = {
+      @CacheEvict(value = "getItem", key = "#p0"), @CacheEvict(value = "getItems", allEntries = true)
+  })
   public void deleteItem(Long itemId) {
     itemRepository.deleteById(itemId);
   }
